7f76cbedb38cf62f94fdfcbee5284be986aacbf6
ankit <ankit.devani@rackspace.com>
2023-09-06 17:48:31 +0530

    added model

---
+++ dags/dbt/jaffle_shop/models/example/stg_customer.sql
@@ -0,0 +1,17 @@
+with
+source as (
+    select * from {{ source('default','jaffle_shop_customers') }}
+),
+
+customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from customers
