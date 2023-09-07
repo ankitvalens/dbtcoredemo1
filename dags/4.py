9d502222d7998303e9c302dcf992067a9c859214
ankit <ankit.devani@rackspace.com>
2023-09-07 15:36:17 +0530

    changes

--- dags/4.py
+++ dags/4.py
@@ -46,7 +46,7 @@ with DAG(
         execution_date = kwargs['execution_date']
         current_date = datetime.now().date()
 
-        if execution_date < current_date:
+        if 12 < 2:
             # Retry the task for the current date
             raise Exception("Task failed for the current date, retrying...")
         elif execution_date < current_date:
