7e57d30e9a471c257cf995ec598286c224591089
ankit <ankit.devani@rackspace.com>
2023-09-07 13:26:54 +0530

    changes

---
+++ dags/jaffle_shop_2.py
@@ -0,0 +1,47 @@
+from pendulum import datetime
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtDag, LoadMode, RenderConfig, DbtTaskGroup, ProfileConfig, ProjectConfig
+from cosmos.profiles import DatabricksTokenProfileMapping
+from cosmos.constants import TestBehavior
+
+#PROJECT_ROOT_PATH="/opt/airflow/git/jaffle_shop.git/dags/dbt/jaffle_shop"  --> managed airflow path
+#PROJECT_ROOT_PATH="/home/gopal/dbt-workspace/jaffle_shop/dags/dbt/jaffle_shop"  --> local development path
+PROJECT_ROOT_PATH=Variable.get("PROJECT_ROOT_PATH")
+
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="default",
+    #If you are using profiles.yml file in git use below profiles_yml_filepath
+    #profiles_yml_filepath=f"{PROJECT_ROOT_PATH}/profiles.yml",
+    #here we are using Airflow connection to provide profile details
+    profile_mapping=DatabricksTokenProfileMapping(
+        conn_id = 'jaffle_shop_databricks_connection',
+    )
+)
+
+with DAG(
+        dag_id="jaffle_shop_dbt_2",
+        start_date=datetime(2023, 2, 6),
+        schedule="@daily",
+        catchup=True,
+        default_args = {
+            'depends_on_past': True
+        }
+):
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    dbt_tg = DbtTaskGroup(
+        project_config=ProjectConfig(PROJECT_ROOT_PATH),
+        profile_config=profile_config,
+        render_config=RenderConfig(
+            select=['tag:customers'],
+        )
+    )
+
+    e2 = EmptyOperator(task_id="post_dbt")
+
+
+    e1 >> dbt_tg >> e2
