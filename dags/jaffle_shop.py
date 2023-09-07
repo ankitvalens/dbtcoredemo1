eaa278c8089d0c5cc41438ee847b5f4be7807a56
ankit <ankit.devani@rackspace.com>
2023-09-07 11:37:39 +0530

    chnages

--- dags/jaffle_shop.py
+++ dags/jaffle_shop.py
@@ -19,7 +19,6 @@ profile_config = ProfileConfig(
     #here we are using Airflow connection to provide profile details
     profile_mapping=DatabricksTokenProfileMapping(
         conn_id = 'jaffle_shop_databricks_connection',
-        profile_args={"schema": "default"},
     )
 )
 
