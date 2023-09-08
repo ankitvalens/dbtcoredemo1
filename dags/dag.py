1448d4b4c8d9c064f20b99ecf77d6069aeab1750
ankit <ankit.devani@rackspace.com>
2023-09-08 15:05:15 +0530

    changes

--- dags/dag.py
+++ dags/dag.py
@@ -21,7 +21,7 @@ profile_config = ProfileConfig(
         conn_id = 'jaffle_shop_databricks_connection',
     )
 )
-
+dbt_var = { "execution_date":execution_date}
 with DAG(
         dag_id="jaffle_shop_dbt_3",
         start_date=datetime(2023, 9, 6),
@@ -38,9 +38,7 @@ with DAG(
             load_method=LoadMode.DBT_MANIFEST
         ),
         operator_args={
-            "vars":{
-                "business_date":datetime(2023, 9, 8)
-            }
+            "vars":dbt_var
         }
     )
 
