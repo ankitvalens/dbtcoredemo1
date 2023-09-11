c1af0d929e207e2cb28ef4d275573b44c83e590f
ankit <ankit.devani@rackspace.com>
2023-09-11 16:56:19 +0530

    changes

--- dags/1.py
+++ dags/1.py
@@ -7,7 +7,6 @@ from cosmos import DbtDag, LoadMode, RenderConfig, DbtTaskGroup, ProfileConfig, 
 from cosmos.profiles import DatabricksTokenProfileMapping
 from cosmos.constants import TestBehavior
 from airflow.contrib.sensors.file_sensor import FileSensor
-from airflow.providers.microsoft.azure.operators.blob_storage import AzureBlobStorageSensor
 
 #PROJECT_ROOT_PATH="/opt/airflow/git/jaffle_shop.git/dags/dbt/jaffle_shop"  --> managed airflow path
 #PROJECT_ROOT_PATH="/home/gopal/dbt-workspace/jaffle_shop/dags/dbt/jaffle_shop"  --> local development path
