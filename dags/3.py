7ab60107e160b8ce2c8c0d461a0c44780cffc47c
ankit <ankit.devani@rackspace.com>
2023-09-25 17:53:24 +0530

    chnages

---
+++ dags/3.py
@@ -0,0 +1,57 @@
+from pendulum import datetime
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.empty import EmptyOperator
+from cosmos import DbtDag, LoadMode, RenderConfig, DbtTaskGroup, ProfileConfig, ProjectConfig
+from cosmos.profiles import DatabricksTokenProfileMapping
+from cosmos.constants import TestBehavior
+import logging
+from airflow.operators.python_operator import PythonOperator
+
+#PROJECT_ROOT_PATH="/opt/airflow/git/jaffle_shop.git/dags/dbt/jaffle_shop"  --> managed airflow path
+#PROJECT_ROOT_PATH="/home/gopal/dbt-workspace/jaffle_shop/dags/dbt/jaffle_shop"  --> local development path
+PROJECT_ROOT_PATH=Variable.get("PROJECT_ROOT_PATH")
+
+profile_config = ProfileConfig(
+    profile_name="jaffle_shop",
+    target_name="dev",
+    #If you are using profiles.yml file in git use below profiles_yml_filepath
+    #profiles_yml_filepath=f"{PROJECT_ROOT_PATH}/profiles.yml",
+    #here we are using Airflow connection to provide profile details
+    profile_mapping=DatabricksTokenProfileMapping(
+        conn_id = 'jaffle_shop_databricks_connection' 
+    )
+)
+
+def my_function(dbt):
+    logging.info(dbt)
+    return "done"
+
+with DAG(
+        dag_id="jaffle_shop_dbt_dfjfn",
+        start_date=datetime(2023, 8, 23),
+        schedule="@daily",
+):
+    e1 = EmptyOperator(task_id="pre_dbt")
+
+    dbt_tg = DbtTaskGroup(
+        project_config=ProjectConfig(dbt_project_path=PROJECT_ROOT_PATH,
+                                     manifest_path=f"{PROJECT_ROOT_PATH}/target/manifest.json",),
+        profile_config=profile_config,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_MANIFEST,
+            #By default cosmos generate dag that execute model and test for that model, if you don't want to use test then pass test_behavior=TestBehavior.NONE
+            #test_behavior=TestBehavior.NONE
+        ),
+    )
+
+    t1 = PythonOperator(
+        task_id='print',
+        python_callable= my_function,
+        op_kwargs = {"dbt_tg" : dbt_tg},
+    ),
+
+    e2 = EmptyOperator(task_id="post_dbt")
+
+    e1 >> dbt_tg >> e2
