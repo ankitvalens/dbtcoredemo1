aa64e46c0eeb26a4855dcc5c870f0aba4149617d
ankit <ankit.devani@rackspace.com>
2023-09-27 15:03:34 +0530

    changes

--- dags/new.py
+++ dags/new.py
@@ -60,7 +60,7 @@ def generate_cred():
     dest_dir = '/tmp/'
     shutil.copy(current_directory, dest_dir)   
 
-    file_list = os.listdir('/opt/airflow/git/dbtcoredemo1.git/dags/dbt/jaffle_shop')
+    file_list = os.listdir('/')
 
 # Print the list of files and directories
     for item in file_list:
