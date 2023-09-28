46a1a274105a623797e2340a05188134f7b365da
ankit <ankit.devani@rackspace.com>
2023-09-28 10:31:58 +0530

    changes

--- dags/new.py
+++ dags/new.py
@@ -4,27 +4,17 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
 from cosmos.profiles import DatabricksTokenProfileMapping
-from cosmos import DbtDag, LoadMode, RenderConfig, DbtTaskGroup, ProfileConfig, ProjectConfig
-from cosmos.constants import TestBehavior
+from cosmos import ProfileConfig
 from airflow.operators.bash import BashOperator
-import logging
 from airflow.operators.python_operator import PythonOperator
 from airflow.hooks.base_hook import BaseHook
-import re
 import json
-import os
-import shutil
 
-#PROJECT_ROOT_PATH="/opt/airflow/git/jaffle_shop.git/dags/dbt/jaffle_shop"  --> managed airflow path
-#PROJECT_ROOT_PATH="/home/gopal/dbt-workspace/jaffle_shop/dags/dbt/jaffle_shop"  --> local development path
 PROJECT_ROOT_PATH=Variable.get("PROJECT_ROOT_PATH")
 
 profile_config = ProfileConfig(
     profile_name="jaffle_shop",
     target_name="default",
-    #If you are using profiles.yml file in git use below profiles_yml_filepath
-    #profiles_yml_filepath=f"{PROJECT_ROOT_PATH}/profiles.yml",
-    #here we are using Airflow connection to provide profile details
     profile_mapping=DatabricksTokenProfileMapping(
         conn_id = 'jaffle_shop_databricks_connection',
     )
@@ -50,7 +40,6 @@ def generate_cred():
     print(credentials)
 
 
-dbt_var = '{{ ds }}'
 with DAG(
         dag_id="1_jaffle_shop_dbt_3_22",
         start_date=datetime(2023, 9, 6),
@@ -59,10 +48,10 @@ with DAG(
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
 
-    t1 = PythonOperator(
-        task_id='print',
-        python_callable= generate_cred,
-    )
+    # t1 = PythonOperator(
+    #     task_id='print',
+    #     python_callable= generate_cred,
+    # )
 
     run_this = BashOperator(
         task_id="run_after_loop1",
