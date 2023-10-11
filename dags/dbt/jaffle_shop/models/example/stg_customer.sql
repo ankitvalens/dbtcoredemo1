e0a9e8c4f5f065acf1e074a057f1b0ca9711ed77
ankit <ankit.devani@rackspace.com>
2023-10-11 11:02:26 +0530

    changes

--- dags/dbt/jaffle_shop/models/example/stg_customer.sql
+++ dags/dbt/jaffle_shop/models/example/stg_customer.sql
@@ -4,7 +4,7 @@ with source1 as (
     select * from {{ source('default','jaffle_shop_customers') }}
 ),
 customers1 as (
-    select * from sourece1
+    select * from source1
 )
 
 select * from customers1
