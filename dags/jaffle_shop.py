14088764912577e5faef249666b27469fe18c9a6
ankit <ankit.devani@rackspace.com>
2024-02-06 15:26:43 +0530

    update dag

--- dags/jaffle_shop.py
+++ dags/jaffle_shop.py
@@ -40,7 +40,7 @@ with DAG(
 
     purview = BashOperator(
         task_id="purview",
-        bash_command=f"dbtlog --env=databricks --dwhcid=jaffle_shop_databricks_connection --azmonitorcid=azure_default",
+        bash_command=f"dbtlog --env=snowflake --dwhcid=snowflake_connection --azmonitorcid=azure_default",
         trigger_rule= "all_done"
     )
 
